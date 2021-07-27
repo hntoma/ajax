0e3898d901d40e9d9477ae40fe81a6c548a9de55
hntoma <hn.toma@gmail.com>
2021-07-28 01:16:32 +0600

    first commit

---
+++ search.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    
+    <meta charset="utf-8">
+    <script type="text/javascript" src="4.js"></script>
+    <h1 align = "center"><u>User List</u></h1>
+ </head>
+
+
+
+
+
+<body>
+
+
+  <br>
+  <br>
+    <form action="2.php" method="post"  onsubmit="return isValid()" name="mForm" id="nForm">
+  
+     
+   <p>
+    
+   <input type="text" name ="Search" placeholder="search by userame">
+         <!--<button type="search" class ="btn" id="Search">Search</button>-->
+         <button onclick="fetchData()">Click</button>
+  
+          <span style="color:red" id="searchErr"> </span>
+        <script>
+
+function fetchData() {
+  var xhttp=new XMLHttpRequest();;
+  
+  //xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    
+    if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("txtHint").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "2.php?q="+str, true);
+  xhttp.send();
+}
+ </script>
+ </p>
+    </div>
+    <table id="table" border=".5" width = "30%" height = "50%">
+
+      <tr>
+<th align="center" style="width:120px;">Id</th>
+<th align="center" style="width:350px;">User Name</th>
+<!--<th align="center" style="width:120px;">Password</th>-->
+</tr>
+
+
+<?php
+
+    $conn=  mysqli_connect("localhost", "root", "12345", "wtg");
+        if(!$conn)
+       {
+           die('not connected');
+       }
+
+
+else{
+
+if($_SERVER['REQUEST_METHOD']==="POST")
+{
+    if(empty($_POST['Search'])){
+        echo"<h4 stlye='colour:red'>Search Field Empty</h4>";
+    }
+  }
+if(isset($_POST['Search']))
+{
+ $searchkey = $_POST['Search'];
+    $sql2 = "SELECT * FROM user WHERE username LIKE '%$searchkey%'";
+ }
+
+ else
+ {
+     $sql2 = "SELECT * FROM user";
+
+}
+}
+
+    $result2 = mysqli_query($conn , $sql2);
+
+  while( $row = mysqli_fetch_assoc($result2) ):
+  ?>
+   
+  <tr>
+              <td align="center"><?php echo $row['id'] ?></td>
+              <td align="center"><?php echo $row['username'] ?></td>
+              <!--<td align="center"><?php echo $row['password'] ?></td>-->
+          </tr>
+<?php endwhile; ?>
+
+    </table>
+      </div>
+      </div>
+
+
+      </form>
+
+  </body>
+</html>
